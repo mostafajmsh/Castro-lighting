292b60ed88f95747c6681f0e19a2762eb551cdd7
mostafa_jmsh <mostafajamshidi7394@gmail.com>
2023-12-27 01:47:07 +0330

    start landing styles

---
+++ js/menu.js
@@ -0,0 +1,68 @@
+function toggleMenu() {
+    const menuIcon = document.querySelector(".menu-icon");
+    const tabletMenu = document.querySelector(".tablet__menu-list");
+    const mobileMenu = document.querySelector(".mobile__menu-list");
+    menuIcon.classList.toggle("menu-open");
+    if (menuIcon.classList.contains("menu-open")) {
+        tabletMenu.style.visibility = "visible";
+        tabletMenu.style.opacity = "1";
+        tabletMenu.style.right = "1.5rem";
+        mobileMenu.style.visibility = "visible";
+        mobileMenu.style.opacity = "1";
+        mobileMenu.style.right = "1.5rem";
+    } else {
+        tabletMenu.style.visibility = "hidden";
+        tabletMenu.style.opacity = "0";
+        tabletMenu.style.right = "-3rem";
+        mobileMenu.style.visibility = "hidden";
+        mobileMenu.style.opacity = "0";
+        mobileMenu.style.right = "-3rem";
+    }
+}
+
+function toggleDrop() {
+    const tabletMenuDropdown = document.querySelector(".menu__drop");
+    const dropdownElem = document.querySelector(".tablet__menu-dropdown");
+    const lastChild = document.querySelector(".last-child");
+    const dropIcon = document.querySelector(".tablet__dropdown-icon");
+
+    tabletMenuDropdown.classList.toggle("menu-open");
+
+    if (tabletMenuDropdown.classList.contains("menu-open")) {
+        dropdownElem.style.visibility = "visible";
+        dropdownElem.style.top = "4rem";
+        dropdownElem.style.opacity = "1";
+        lastChild.style.margin = "270px 0 0 0";
+        dropIcon.style.transform = "rotate(180deg)";
+    } else {
+        dropdownElem.style.visibility = "hidden";
+        dropdownElem.style.top = "-15rem";
+        dropdownElem.style.opacity = "0";
+        lastChild.style.margin = "0";
+        dropIcon.style.transform = "none";
+    }
+}
+
+function mobileToggleDrop() {
+
+    const tabletMenuDropdown = document.querySelector(".menu__drop");
+    const dropdownElem = document.querySelector(".mobile__menu-dropdown");
+    const lastChild = document.querySelector(".mobile__last-child");
+    const dropIcon = document.querySelector(".mobile__dropdown-icon");
+
+    tabletMenuDropdown.classList.toggle("menu-open");
+
+    if (tabletMenuDropdown.classList.contains("menu-open")) {
+        dropdownElem.style.visibility = "visible";
+        dropdownElem.style.top = "4rem";
+        dropdownElem.style.opacity = "1";
+        lastChild.style.margin = "220px 0 0 0";
+        dropIcon.style.transform = "rotate(180deg)";
+    } else {
+        dropdownElem.style.visibility = "hidden";
+        dropdownElem.style.top = "-15rem";
+        dropdownElem.style.opacity = "0";
+        lastChild.style.margin = "0";
+        dropIcon.style.transform = "none";
+    }
+}
